8b7bc653c4988b62b900b28dca4f58c34911dbac
Deepak Thankachan <deepakthankachan@Deepaks-iMac.local>
2018-09-02 23:18:46 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,11 +22,20 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos)=>{
+        console.log(todos);
+        res.send({todos})
+    }, (err)=>{
+        console.log(err);
+        res.status(400).send(e);
+    })
+});
 
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
-})
+});
 
 
 module.exports = {
