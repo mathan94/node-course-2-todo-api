ce24c03fdf761a4ee87089ef913b0449639c6bea
Deepak Thankachan <deepakthankachan@Deepaks-iMac.local>
2018-09-03 13:58:14 +0100

    Add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo')
@@ -32,11 +33,30 @@ app.get('/todos', (req, res) => {
     })
 });
 
+// GET /todos/1234324
+
+app.get('/todos/:id', (req, res) => {
+    id = req.params.id
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+    Todo.findById(id).then((todo)=>{
+
+        if(todo){
+            return res.send({todo});
+        }else{
+            return  res.status(404).send();
+        }
+    }).catch(e=>{
+        res.status(400).send(e);
+    })
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
-
 
 module.exports = {
     app
